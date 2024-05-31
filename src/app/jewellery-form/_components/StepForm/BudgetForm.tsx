8e3437a411d806bf039ebe39acb85b1fc465e131
Shivansh mishra <Shivanshmishra.chd@gmail.com>
2024-06-01 03:00:43 +0530

    GenderForm.tsx -> Replace default <form> element with shadcn-ui's Form and use zod with react-hook-form to handle form

--- src/app/jewellery-form/_components/StepForm/BudgetForm.tsx
+++ src/app/jewellery-form/_components/StepForm/BudgetForm.tsx
@@ -1,27 +1,77 @@
-import React from "react";
-import { IStep } from "../../_utils/IStep";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { FormNavigationButton } from "./FormNavigationButton";
-import { Separator } from "@/components/ui/separator";
+"use client";
 
-export function BudgetForm(step: IStep) {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { toast } from "@/components/ui/use-toast";
+import { FormNavigationButton } from "./FormNavigationButton";
+
+const budgetGroups: string[] =  ["<$50", "$50-$100", "$100-$200", "$200+"];
+const FormSchema = z.object({
+  type: z.enum( ["<$50", "$50-$100", "$100-$200", "$200+"], {
+    required_error: "You need to select an age group.",
+  }),
+});
+
+export function BudgetForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onFormSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <form>
-      <RadioGroup defaultValue="18-25" required>
-        <div className="flex gap-4 flex-wrap flex-col  justify-start">
-            {
-              step.options.map(option => <Button key={option} type="button" variant={"secondary"} asChild>
-                <Label htmlFor={option} className="space-x-2">
-                  <RadioGroupItem value={option} id={option} />
-                  <span>{option}</span>
-                </Label>
-              </Button>)
-            }
-        </div>
-      </RadioGroup>
-      <FormNavigationButton />
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onFormSubmit)} className="">
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <RadioGroup
+                  defaultValue={field.value}
+                  onValueChange={field.onChange}
+                  className="flex gap-4 flex-wrap flex-col  justify-start"
+                >
+                  {
+                    budgetGroups.map(group => (
+                      <FormItem className="space-x-2 cursor-pointer p-1">
+                        <FormControl>
+                          <RadioGroupItem value={group} />
+                        </FormControl>
+                        <FormLabel className="cursor-pointer">{group}</FormLabel>
+                      </FormItem>
+                    ))
+                  }
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormNavigationButton />
+      </form>
+    </Form>
   );
 }
--- src/app/jewellery-form/_components/StepForm/GenderFrom.tsx
+++ src/app/jewellery-form/_components/StepForm/GenderFrom.tsx
@@ -8,49 +8,80 @@ import { cn } from "@/lib/utils";
 import { useState } from "react";
 import { FormNavigationButton } from "./FormNavigationButton";
 import { IoMale, IoMaleFemale, IoFemale } from "react-icons/io5";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+const FormSchema = z.object({
+  genders: z.enum(["male", "female", "other"], {
+    required_error: "Please select one.",
+  }),
+});
+
 const genderOptions = [
-  { option: "male", icon: <IoMale className="size-[60px]"/> },
-  { option: "female", icon: <IoFemale className="size-[60px]"/> },
-  { option: "other", icon: <IoMaleFemale className="size-[60px]"/> },
+  { option: "male", icon: <IoMale className="size-[60px]" /> },
+  { option: "female", icon: <IoFemale className="size-[60px]" /> },
+  { option: "other", icon: <IoMaleFemale className="size-[60px]" /> },
 ];
 
-
 export function GenderFrom(step: IStep) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onFormSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
   const [selectedOption, setSelectedOption] = useState("male");
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <div className="flex flex-wrap justify-evenly gap-4">
-        {genderOptions.map(({ icon, option }) => (
-          <Button
-            type="button"
-            variant={"outline"}
-            asChild
-            className="active:scale-90"
-          >
-            <Label
-              htmlFor={option}
-              className={cn(
-                "flex flex-col gap-2 justify-center items-center cursor-pointer p-4 border flex-shrink-0 size-36 rounded-2xl",
-                selectedOption === option && "bg-blue-600 text-white"
-              )}
-            >
-              <Input
-                type="radio"
-                name="gender"
-                id={option}
-                checked={selectedOption === option}
-                onChange={() => setSelectedOption(option)}
-                className="hidden"
-              />
-              <div>
-                {icon}
-              </div>
-              <p className="inline-block capitalize">{option}</p>
-            </Label>
-          </Button>
-        ))}
-      </div>
-      <FormNavigationButton />
-    </form>
+    <Form {...form}>
+      <form>
+        <FormField
+          control={form.control}
+          name="genders"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <RadioGroup
+                  defaultValue={field.value}
+                  onValueChange={field.onChange}
+                  className="flex flex-wrap gap-4"
+                >
+                  {genderOptions.map(({ option, icon }) => (
+                    <Button
+                      type="button"
+                      variant={"outline"}
+                      asChild
+                      className="active:scale-90"
+                    >
+                      <FormLabel
+                        htmlFor={option}
+                        className={cn(
+                          "flex flex-col gap-2 justify-center items-center cursor-pointer p-4 border flex-shrink-0 size-36 rounded-2xl",
+                          field.value === "male" && "bg-blue-600 text-white"
+                        )}
+                      >
+                        <RadioGroupItem value={option} className="hidden" />
+                        <div>{icon}</div>
+                        <p className="inline-block capitalize">{option}</p>
+                      </FormLabel>
+                    </Button>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormNavigationButton />
+      </form>
+    </Form>
   );
 }
